34264867c5242c8cbc33a505bdd93535c954a456
Felix Saiz Von Fraunberg <5555415001@edu.gencat.cat>
2022-02-22 11:46:35 +0100

    added an other socket test and fonts folder with a links.txt file

--- sockets_tests/test_1_client.py
+++ sockets_tests/test_1_client.py
@@ -6,11 +6,9 @@ port = 12345
 
 print(host)
 
-host = "127.0.0.1"
-
 # connect to host
 s.connect((host, port))
- 
+
 # recv message and decode here 1024 is buffer size.   
 print (s.recv(1024).decode())  
 s.close()
